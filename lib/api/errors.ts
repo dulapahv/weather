8f1e21b224a36db7ce8e7891849777121e7c2ec0
Dulapah Vibulsanti <dulapah6@gmail.com>
2026-07-02 21:36:24 +0100

    refactor: restructure folder

--- lib/api/errors.ts
+++
@@ -1,9 +0,0 @@
-export class UpstreamError extends Error {
-  constructor(
-    public readonly status: number,
-    message: string
-  ) {
-    super(message);
-    this.name = 'UpstreamError';
-  }
-}
